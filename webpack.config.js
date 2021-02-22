bbe20a7cb65d39696b185f1abb3561b931415782
Cyril Wanner <info@cyr.li>
2021-02-22 10:41:51 +0000

    Setup project with typescript and webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'babel-loader',
+        include: path.resolve(__dirname, 'src'),
+      },
+    ],
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
